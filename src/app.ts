b5633c9f412b5deb3b4e467f47793385a4a183b0
Thiago Martins <thiagopmartins@outlook.com>
2020-03-08 19:57:29 -0300

    adicionando module-alias

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import 'module-alias/register';
+
 import routes from './routes';
 
 class App {
@@ -21,7 +23,7 @@ class App {
     this.express.use(express.json());
     this.express.use(cors());
     this.express.options('*', cors());
-  } 
+  }
 
   private database(): void {
     mongoose.connect(`${process.env.MONGO_URL}`, {
